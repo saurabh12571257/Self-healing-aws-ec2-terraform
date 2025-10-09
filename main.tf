fff76f0d6aa9099e4457cb3f2c3cb817e3a76a79
saurabh12571257 <davesaurabh59@gmail.com>
2025-10-09 21:27:04 +0530

    Added lamda packaging

--- main.tf
+++ main.tf
@@ -40,3 +40,61 @@ data "aws_ami" "ubuntu" {
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  output_path = "./lambda_function.zip"
+
+
+  source {
+    content  = <<PY
+import json
+import boto3
+import urllib.request
+
+
+EC2 = boto3.client('ec2')
+
+
+def lambda_handler(event, context):
+    body = event.get('body')
+    try:
+        payload = json.loads(body)
+        alerts = payload.get('alerts', [])
+    except Exception:
+        return {"statusCode":400, "body":"invalid payload"}
+
+
+    for alert in alerts:
+        instance_label = alert.get('labels', {}).get('instance', '')
+        # Expect instance_label to be private IP (e.g., 10.0.x.x)
+        instance_id = get_instance_id_by_private_ip(instance_label)
+        if instance_id:
+            try:
+                EC2.reboot_instances(InstanceIds=[instance_id])
+            except Exception as e:
+                print('reboot failed', e)
+        else:
+            print('no instance mapping for', instance_label)
+
+
+    return {"statusCode":200, "body":"ok"}
+
+
+
+
+def get_instance_id_by_private_ip(private_ip):
+    try:
+        resp = EC2.describe_instances(Filters=[{'Name':'private-ip-address','Values':[private_ip]}])
+        for r in resp.get('Reservations',[]):
+            for i in r.get('Instances',[]):
+                return i.get('InstanceId')
+    except Exception as e:
+        print(e)
+    return None
+
+
+PY
+    filename = "lambda_function.py"
+  }
+}
--- s3.tf
+++ s3.tf
@@ -2,3 +2,11 @@ resource "aws_s3_bucket_acl" "lambda_bucket" {
   bucket = "${local.app_name}-lambda-artifacts-${random_id.bucket_suffix.hex}"
   acl    = "private"
 }
+
+resource "aws_s3_object" "lambda_artifact" {
+  bucket       = aws_s3_bucket_acl.lambda_bucket.id
+  key          = "lambda/lambda_function.zip"
+  source       = data.archive_file.lambda_zip.output_path
+  etag         = filemd5(data.archive_file.lambda_zip.output_path)
+  content_type = "application/zip"
+}
